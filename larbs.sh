c3105b3b4c303084eb21f7fda79ad758266e3f5f
Faisal Richard D. Tamano Jr <84402344+FaisalTamanoJr@users.noreply.github.com>
2021-11-12 10:23:26 +0000

    Changed the dotfiles and programs to its forks

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/FaisalTamanoJr/dotfiles.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/FaisalTamanoJr/install_script/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
